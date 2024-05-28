04aa8cf8df5a0c9c37817fcc7d62818562f42a4a
Михаил <41448637+mkgrgis@users.noreply.github.com>
2024-05-28 20:48:43 +0300

    Ускорение бассейнового запроса

--- SQL/3 Бассейны.sql
+++ SQL/3 Бассейны.sql
@@ -26,66 +26,88 @@ INSERT INTO "Павловский парк"."Расчётные стоки" (id,
 INSERT INTO "Павловский парк"."Расчётные стоки" (id, osm_id, osm_type, "Название бассейна") VALUES(15, 1056508708, 'way', 'ББ Мавзолейный ручей в Белой Берёзе');
 INSERT INTO "Павловский парк"."Расчётные стоки" (id, osm_id, osm_type, "Название бассейна") VALUES(16, 407877612, 'way', 'ББ Розовопавильонный дренаж');
 
+drop table "Павловский парк"."Бассейны" cascade;
+
 create table "Павловский парк"."Бассейны" as
-select 0::smallint "bas_id", "osm_id", "osm_type", geom "Бассейн", geom "ini", 0::int2 "Шаг"
+select 0::smallint "bas_id", "osm_type", "osm_id", geom "Бассейн", geom "ini", 0::int2 "Шаг"
   from "Павловский парк"."Водотоки линейные"
  where false ;
-  
+ALTER TABLE "Павловский парк"."Бассейны" ADD CONSTRAINT "Бассейны_unique" UNIQUE (osm_type,osm_id);
+ALTER TABLE "Павловский парк"."Бассейны" ADD CONSTRAINT "Бассейны_pk" PRIMARY KEY (bas_id);
+ALTER TABLE "Павловский парк"."Бассейны" ADD CONSTRAINT "Бассейны_Расчётные_стоки_FK" FOREIGN KEY (bas_id) REFERENCES "Павловский парк"."Расчётные стоки"(id);
+
+drop table "Павловский парк"."Бассейны использованы" cascade;
+
 create table "Павловский парк"."Бассейны использованы" (
-"bas_id" smallint,
-"osm_id" int8,
-"osm_type" varchar(16)
+"bas_id" smallint not null,
+"osm_type" varchar(16) not null,
+"osm_id" int8 not null,
+"Шаг" smallint null
 );
 ALTER TABLE "Павловский парк"."Бассейны использованы" ADD CONSTRAINT "Бассейны_использованы_pk" PRIMARY KEY (bas_id,osm_type,osm_id);
 
 truncate table "Павловский парк"."Бассейны";
 truncate table "Павловский парк"."Бассейны использованы";
 
-create or replace view "Павловский парк"."Бассейны+1" as
-select st_union(b."Бассейн", r.geom) "Бассейн",       
-       b.bas_id,
-       r.*
+drop view "Павловский парк"."Бассейны+1" cascade;
+create view "Павловский парк"."Бассейны+1" as
+with u as
+(select st_union(b."Бассейн", o.geom) "Бассейн",
+       b.bas_id,       
+       o.osm_type,
+       o.osm_id,
+       o.tags,
+       o.geom,
+       o.geom_type,
+       b."Шаг"
   from "Павловский парк"."Бассейны" b
- inner join "Павловский парк"."OSM ∀" r -- Очередной примыкающий водоток
-    on (r.osm_id, r.osm_type, b.bas_id ) not in (select osm_id, osm_type, bas_id
-                                         from "Павловский парк"."Бассейны использованы" bi)
-   and (
-       ( r.geom_type = 'ST_LineString'
-   and r.tags ->> 'waterway' is not null
-   and r.tags ->> 'bridge' is null
-   and st_intersects(r.geom, b."Бассейн")
-   and not st_intersects(st_startpoint(r.geom), b."Бассейн")
-   and not st_intersects(r.geom, b.ini )
+ inner join "Павловский парк"."OSM ∀" o
+    on (
+       ( o.geom_type = 'ST_LineString'
+   and o.tags ->> 'waterway' is not null
+   and o.tags ->> 'bridge' is null
+   and o.tags ->> 'tunnel' is null   
+   and st_intersects(o.geom, b."Бассейн")
+   and not st_intersects(st_startpoint(o.geom), b."Бассейн")
+   and not st_intersects(o.geom, b.ini )
        )
-    or ( r.geom_type = 'ST_LineString'
-   and r.tags ->> 'waterway' is not null
-   and r.tags ->> 'bridge' is not null
-   and st_intersects(st_endpoint(r.geom), b."Бассейн")
+    or ( o.geom_type = 'ST_LineString'
+   and o.tags ->> 'waterway' is not null
+   and (o.tags ->> 'bridge' is not null -- Акведуки
+    or o.tags ->> 'tunnel' is not null )-- Подземные водоводы
+   and st_intersects(st_endpoint(o.geom), b."Бассейн")
        )
-    or ( r.geom_type != 'ST_LineString'
-   and r.tags ->> 'natural' = 'water'
-   and st_intersects(r.geom, b."Бассейн")
-   and not st_intersects(r.geom, b."ini")       
+    or ( o.geom_type != 'ST_LineString' -- Водные глади
+   and o.tags ->> 'natural' = 'water'
+   and st_intersects(o.geom, b."Бассейн")
+   and not st_intersects(o.geom, b."ini")       
        )
        )
-   and (r.osm_id, r.osm_type) != (b.osm_id, b.osm_type);
--- Создание объектов завершено
-
+   and (o.osm_type, o.osm_id) != (b.osm_type, b.osm_id)
+   )
+  select "Бассейн", u."Шаг", bas_id, osm_type, osm_id, u.tags, u.geom, u.geom_type, i.bas_id "Новый" 
+  from u
+  left join "Павловский парк"."Бассейны использованы" i
+  using (bas_id, osm_type, osm_id)    
+/*
+  where (b.bas_id, o.osm_id, o.osm_type) not in 
+  (select bas_id, osm_type, osm_id from "Павловский парк"."Бассейны использованы"       
+  ) */;
+                                    
 -- Первичное заполнение
-insert into "Павловский парк"."Бассейны"
+insert into "Павловский парк"."Бассейны" ("bas_id", "osm_type", "osm_id", "Бассейн", "ini", "Шаг")
 with b as (
 select r.id "bas_id",
        "osm_id",
        "osm_type",
        geom "Бассейн",
        (st_dump(geom)).geom "ini",       
-       0::smallint "Шаг",
        o.geom_type
   from "Павловский парк"."Расчётные стоки" r
   join "Павловский парк"."OSM ∀" o
 using ("osm_type", "osm_id") 
 ),
-b1 as (
+ini as (
 select "bas_id",
        "osm_type",
        "osm_id",       
@@ -95,36 +117,35 @@ select "bas_id",
             when st_geometrytype("ini") = 'ST_Point'
             then "ini"
             else st_boundary("ini") 
-       end "ini",       
-       "Шаг"
+       end "ini"       
   from b
  )
 select "bas_id",
-       "osm_id",
        "osm_type",
+       "osm_id",       
        st_union("Бассейн"),
        st_union("ini"),       
-       "Шаг"
-  from b1
+       0::smallint "Шаг"
+  from ini
+  where bas_id = 15 
   group by "bas_id", "osm_type", "osm_id", "Шаг"
   ;
                                  
  MERGE INTO "Павловский парк"."Бассейны" b
- USING ( select ST_Union("Бассейн") "Бассейн", "bas_id"
+ USING ( select ST_Union(geom) "Бассейн+", "bas_id"
            from "Павловский парк"."Бассейны+1"
+          -- where "Новый" 
           group by bas_id ) v
     ON v.bas_id = b.bas_id
-   and not ST_Equals(b."Бассейн", v."Бассейн")
+   and not ST_Equals(b."Бассейн", ST_Union(b."Бассейн", v."Бассейн+"))
   WHEN MATCHED then
-UPDATE SET "Бассейн" = ST_Union(b."Бассейн", v."Бассейн"), "Шаг" = "Шаг" + 1;
+UPDATE SET "Бассейн" = ST_Union(b."Бассейн", v."Бассейн+"), "Шаг" = "Шаг" + 1;
                 
 INSERT INTO "Павловский парк"."Бассейны использованы"
-       (bas_id, osm_id, osm_type)
-select distinct bas_id, osm_id, osm_type
-  from "Павловский парк"."Бассейны+1"
-  where (bas_id, osm_id, osm_type) not in 
-  (select bas_id, osm_id, osm_type 
-     from "Павловский парк"."Бассейны использованы")
+       (bas_id, osm_type, osm_id, "Шаг")
+select bas_id, osm_type, osm_id, "Шаг"
+  from "Павловский парк"."Бассейны+1" where (bas_id, osm_type, osm_id) not in 
+  (select bas_id, osm_type, osm_id from "Павловский парк"."Бассейны использованы")
 --returning * ;
 
 create view "Павловский парк"."Бассейны geoJSON" as 
